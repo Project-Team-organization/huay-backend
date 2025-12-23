8488cc2fb7999b58ae8624d4973c7f223b0ead08
Warunyoo <warunyoo084@gmail.com>
2025-12-23 11:42:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,18 @@ async function startServer() {
   });
 
   // Middleware
-  app.use(cors());
+  app.use(cors({
+    origin: [
+      'https://admin.kaojing.online',
+      'https://asia.kaojing.online',
+      'https://kaojing.online',
+      'http://localhost:3000',
+      'http://localhost:5173'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    credentials: true
+  }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(fileUpload());
