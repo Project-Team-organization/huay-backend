665174e7cf32381b5ef0d6a87b549d241ba9c08d
natthanon_sornsuwan <64368695+nonin22222@users.noreply.github.com>
2026-01-26 12:00:34 +0700

    Refactor master routes for readability and update middleware
    Reformatted route definitions for better readability by using multi-line syntax. Updated the '/getcustomer/:id' route to use 'authmiddleware.isMaster' instead of 'permissionmanagermaster'.

--- routes/master.routes.js
+++ routes/master.routes.js
@@ -3,14 +3,45 @@ const router = express.Router();
 const masterController = require("../controller/master/master.controller");
 const authmiddleware = require("../middleware/authadmin.middleware");
 
+router.post(
+  "/create",
+  authmiddleware.permissionmanagermaster,
+  masterController.createMaster,
+);
+router.get(
+  "/get",
+  authmiddleware.permissionmanagermaster,
+  masterController.getAllMasters,
+);
+router.get(
+  "/getbyid/:id",
+  authmiddleware.permissionmanagermaster,
+  masterController.getMasterById,
+);
+router.put(
+  "/update/:id",
+  authmiddleware.permissionmanagermaster,
+  masterController.updateMaster,
+);
+router.delete(
+  "/delete/:id",
+  authmiddleware.permissionmanagermaster,
+  masterController.deleteMaster,
+);
+router.put(
+  "/active/:id",
+  authmiddleware.permissionmanagermaster,
+  masterController.activateMaster,
+);
+router.put(
+  "/deactive/:id",
+  authmiddleware.permissionmanagermaster,
+  masterController.deactivateMaster,
+);
 
-router.post("/create", authmiddleware.permissionmanagermaster, masterController.createMaster);
-router.get("/get", authmiddleware.permissionmanagermaster, masterController.getAllMasters);
-router.get("/getbyid/:id", authmiddleware.permissionmanagermaster, masterController.getMasterById);
-router.put("/update/:id", authmiddleware.permissionmanagermaster, masterController.updateMaster);
-router.delete("/delete/:id", authmiddleware.permissionmanagermaster, masterController.deleteMaster);
-router.put("/active/:id", authmiddleware.permissionmanagermaster, masterController.activateMaster);
-router.put("/deactive/:id", authmiddleware.permissionmanagermaster, masterController.deactivateMaster);
-
-router.get("/getcustomer/:id", authmiddleware.permissionmanagermaster, masterController.getCustomerByMaster);
+router.get(
+  "/getcustomer/:id",
+  authmiddleware.isMaster,
+  masterController.getCustomerByMaster,
+);
 module.exports = router;
