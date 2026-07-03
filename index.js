2546d204e64c90995929d32f7ede1539f4d60234
natthanon_sornsuwan <64368695+nonin22222@users.noreply.github.com>
2026-07-04 02:55:10 +0700

    Disable cronjob imports in index.js
    Commented out require statements that load cronjob modules (huay.lao, huay.thai, huay.magnum4d, huay.hanoi, cronjob_set, cleanup.logs) so scheduled tasks are not registered at application startup. This temporarily disables background jobs for development or controlled deployments; uncomment the lines to re-enable.

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const routes = require("./routes");
 
 // Import cron job functions
 
-require("./cronjob/huay.lao");
-require("./cronjob/huay.thai");
-require("./cronjob/huay.magnum4d");
-require("./cronjob/huay.hanoi");
-require("./cronjob/cronjob_set");
-require("./cronjob/cleanup.logs");
+// require("./cronjob/huay.lao");
+// require("./cronjob/huay.thai");
+// require("./cronjob/huay.magnum4d");
+// require("./cronjob/huay.hanoi");
+// require("./cronjob/cronjob_set");
+// require("./cronjob/cleanup.logs");
 // require('./cronjob/huay.foreign.stock');
 
 async function startServer() {
