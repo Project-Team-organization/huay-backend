e5f5ad9769dc81601ea52f5617d837286cd80204
warunyoo1 <151112524+warunyoo1@users.noreply.github.com>
2026-07-10 19:42:48 +0700

    [backend]Update

--- index.js
+++ index.js
@@ -9,14 +9,8 @@ const config = require("./config/config");
 const routes = require("./routes");
 
 // Import cron job functions
-
-require("./cronjob/huay.lao");
-require("./cronjob/huay.thai");
-require("./cronjob/huay.magnum4d");
-require("./cronjob/huay.hanoi");
 require("./cronjob/cronjob_set");
 require("./cronjob/cleanup.logs");
-// require('./cronjob/huay.foreign.stock');
 
 async function startServer() {
   const app = express();
